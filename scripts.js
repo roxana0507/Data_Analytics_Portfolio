f61d7f78ad5ade3fe488ccd253439d9cdd55b574
Roxana Chinchilla <roxy.0507@gmail.com>
2025-05-31 19:18:14 -0600

    Agregada página de detalles del Proyecto 1

--- scripts.js
+++ scripts.js
@@ -155,3 +155,13 @@ particlesJS('particles-js', {
   observer.observe(flipCard);
 });
 
+
+  function openImage(src) {
+    document.getElementById("lightbox-img").src = src;
+    document.getElementById("lightbox").style.display = "flex";
+  }
+
+  function closeImage() {
+    document.getElementById("lightbox").style.display = "none";
+  }
+
